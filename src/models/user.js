3b4cd95e73d71258befdfb8f860d1c0b5b5324d3
hcgaron <hcgaron@gmail.com>
2019-08-16 22:37:04 -0400

    init commit

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// create a schema for our user model so we can use middleware
+// middleware will allow us to access hooks to do things like hash passwords
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password must not contain "password"');
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
+  }
+})
+
+
+const User = mongoose.model('User', )
+
+module.exports = User;
---
+++ src/routers/task.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const Task = require('../models/task');
+const router = new express.Router();
+
+// task creation endpoint (C in CRUD) for REST API
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+      await task.save();
+      res.status(201).send(task);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  })
+  
+  
+  // fetch multiple tasks endpoint (R in CRUD) for REST API
+  router.get('/tasks', async (req, res) => {
+    try {
+      const tasks = await Task.find({});
+      res.send(tasks)
+    } catch (error) {
+      res.status(500).send();
+    }
+  })
+  
+  
+  // fetch individual task by ID endpoint (R in CRUD) for REST API
+  router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+      const task = await Task.findById(_id);
+      if (!task) {
+        return res.status(401).send();
+      }
+  
+      res.send(task);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  })
+  
+  // update a task by ID (U in CRUD) for REST API
+  router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdated = ['description', 'completed'];
+    const isValidUpdate = updates.every((update) => {
+      return allowedUpdated.includes(update);
+    })
+  
+    if (!isValidUpdate) {
+      return res.status(400).send({error: 'Invalid update!'})
+    }
+  
+    try {
+      const task = await Task.findByIdAndUpdate(req.params.id, req.body, 
+        {
+          new: true,
+          runValidators: true
+        })
+  
+        if (!task) {
+          return res.status(404).send();
+        }
+  
+        res.send(task);
+    } catch (error) {
+      return res.status(400).send();
+    }
+  
+  })
+  
+  // delete a task (D in CRUD) for REST API
+  router.delete('/tasks/:id', async (req, res) => {
+    try {
+      const task = await Task.findByIdAndDelete(req.params.id);
+  
+      if (!task) {
+        res.send(404).send();
+      }
+  
+      res.send(task);
+    } catch (error) {
+      res.status(500).send();
+    }
+  })
+
+  module.exports = router;
